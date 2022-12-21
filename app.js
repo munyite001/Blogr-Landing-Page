08a97aa4349a321fc38b6557977e4f79f359d7c1
Emmanuel Munyite <emunyite@gmail.com>
2022-12-21 18:57:58 +0300

    Mobile menu functionality with javascript

---
+++ app.js
@@ -0,0 +1,58 @@
+//  Mobile - menu
+const mobileMenu = document.querySelector('.mobile-menu');
+const openMenu = document.querySelector('.hamburger-menu');
+const hamburger = document.querySelector('.open-menu-img');
+const menuLinks = document.querySelectorAll('.mobile-menu-link');
+const nav = document.querySelector('.nav');
+
+openMenu.addEventListener('click', ()=>{
+  if (hamburger.classList.contains('open'))
+  {
+    hamburger.classList.remove('open');
+    hamburger.src = "./images/icon-close.svg";
+    mobileMenu.classList.add('show-mobile-menu');
+  }
+  else 
+  {
+    hamburger.classList.add('open');
+    hamburger.src = "./images/icon-hamburger.svg";
+    mobileMenu.classList.remove('show-mobile-menu');
+  }
+  
+});
+
+//  Show and hide Mobile menu lists
+menuLinks.forEach((link) => {
+  link.addEventListener('click',(e) => {
+    let menuList = e.currentTarget.querySelector('.menu-link-list');
+    let arrow = e.currentTarget.querySelector('.arrow-icon');
+    if(menuList.classList.contains("show-link-list"))
+    {
+      menuList.classList.remove("show-link-list");
+      arrow.style.rotate = "0deg";
+    }
+    else
+    {
+      menuList.classList.add("show-link-list");
+      arrow.style.rotate = "180deg";
+    }
+  });
+});
+
+
+//  Sticky Navbar past a certain point
+window.addEventListener("scroll", () => {
+  if (scrollY > 40)
+  {
+    nav.classList.add('sticky-nav');
+    nav.style.width = "100%";
+  }
+  else
+  {
+    nav.classList.remove('sticky-nav');
+    if (window.innerWidth > 768)
+    {
+      nav.style.width = "80%";
+    }
+  }
+})
